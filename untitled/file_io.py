68409bcbda4eec0c825f4169f6863ec8a23c178c
MAJIN123 <806379655@qq.com>
2017-03-10 19:35:31 +0800

    python

---
+++ untitled/file_io.py
@@ -0,0 +1,19 @@
+#coding:utf-8
+import os
+##读文件的字符串到sts（是个list）
+filename=('1','2','3','4','5','6','7','8','9','10')
+sts=[]
+for i in range(10):
+    with open (filename[i],'r') as f:
+        lines=f.readlines()
+        for line in lines:
+            for st in line.split():
+                sts.append(st)
+db={}
+for word in sts:
+    if word in db:
+        db[word]=db[word]+1
+    else:
+        db[word]=1
+print(db)
+
